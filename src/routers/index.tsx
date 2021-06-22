f3248046438401a649e659c345c4e55d43e650d2
Jos3s <karatedkgt17@gmail.com>
2021-06-22 12:33:04 -0300

    Removendo importação do react

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,14 +1,12 @@
-import { ReactNode } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import { NewRoom } from '../pages/NewRoom';
 
-
 export const Routes = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
+      <Route path="/" exact component={Home}/>
       <Route path="/rooms/new" exact component={NewRoom} />
     </BrowserRouter>
   )
