f1242845f9589c180eaba0bb2fb343f453a74990
Jos3s <karatedkgt17@gmail.com>
2021-06-21 23:25:24 -0300

    Atualizando exportação do firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -16,5 +16,7 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-export const auth=firebase.auth();
-export const database=firebase.database();
+const auth = firebase.auth();
+const database = firebase.database();
+
+export { firebase, auth, database };
