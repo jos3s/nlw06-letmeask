c4e36336f632341bb36495872d986ba07d76e993
Jos3s <karatedkgt17@gmail.com>
2021-06-23 09:45:59 -0300

    Mudando cor do warning toast

--- src/components/Toast/styles.ts
+++ src/components/Toast/styles.ts
@@ -26,7 +26,7 @@ export const Container = styled.div`
   font-weight:500;
   color:${({ theme }) => theme.colors.white.light};
   min-width: 250px; 
-  background-color: ${({ theme, className }) => className ==="info" ? theme.colors.purple.light : className ==="error" ? theme.colors.danger : "#FFC107"};
+  background-color: ${({ theme, className }) => className ==="info" ? theme.colors.purple.light : className ==="error" ? theme.colors.danger : "#FFA000"};
   color: #fff; 
   text-align: center; 
   border-radius: ${({ theme }) => theme.borderRadius.md} 0 0 ${({ theme }) => theme.borderRadius.md}; /* Rounded borders */
