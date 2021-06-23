a3ca1c220fc54dd8351ebbd9e03a2f382c1eb7d6
Jos3s <karatedkgt17@gmail.com>
2021-06-23 00:10:25 -0300

    Definindo width

--- src/components/Letmeask/styles.ts
+++ src/components/Letmeask/styles.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+  width:100%;
   color:${({ theme })=>theme.title==="dark" ? theme.colors.white.light : theme.colors.black};
 `;
 
