6f51b23bfcce21e61a592de751e68537918c9628
Jos3s <karatedkgt17@gmail.com>
2021-06-23 00:12:06 -0300

    Nova rota para determinada sala

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -1,13 +1,17 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import { Home } from '../pages/Home';
 import { NewRoom } from '../pages/NewRoom';
+import { Room } from '../pages/Room';
 
 export const Routes = () => {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home}/>
-      <Route path="/rooms/new" exact component={NewRoom} />
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/rooms/new" exact component={NewRoom} />
+        <Route path="/rooms/:id" component={Room} />
+      </Switch> 
     </BrowserRouter>
   )
 }
