dddeff1cbe748288ac40298c9086f598c578b118
Jos3s <karatedkgt17@gmail.com>
2021-06-23 20:45:51 -0300

    Adicionando rota para a página de admin

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Home } from '../pages/Home';
 import { NewRoom } from '../pages/NewRoom';
 import { Room } from '../pages/Room';
+import { AdminRoom } from '../pages/AdminRoom';
 
 export const Routes = () => {
   return (
@@ -11,6 +12,7 @@ export const Routes = () => {
         <Route path="/" exact component={Home}/>
         <Route path="/rooms/new" exact component={NewRoom} />
         <Route path="/rooms/:id" component={Room} />
+        <Route path="/admin/rooms/:id" component={AdminRoom} />
       </Switch> 
     </BrowserRouter>
   )
