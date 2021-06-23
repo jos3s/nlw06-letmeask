3396793de729012126e0e418e0150de671799d3a
Jos3s <karatedkgt17@gmail.com>
2021-06-23 00:10:15 -0300

    Cor de fundo com base no tema

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -4,9 +4,9 @@ export const Input = styled.input`
   height:50px;
   border-radius:${({ theme }) => theme.borderRadius.md};
   padding:0 ${({ theme }) => theme.spacings.md};
-  background-color:transparent;
+  background-color:${({ theme }) =>  theme.title ==="dark" ? theme.colors.bg.input : theme.colors.white.light};
   color:${({ theme }) => theme.title === "light" ? theme.colors.purple.light : theme.colors.white.light};
-  border:1px solid ${({ theme }) => theme.colors.gray.medium};
+  border: ${({ theme }) =>theme.title === "light" ? theme.colors.gray.medium +" .1em solid" : "none"};
   width:100%;
 
   &:focus{
